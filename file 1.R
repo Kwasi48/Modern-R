ab95a9cb7fac17ad787b3bf65220e35b9c0705f4
Kwasi48 <abuwestwood@gmail.com>
2024-10-15 02:28:50 +0000

    time series

--- file 1.R
+++ file 1.R
@@ -3,5 +3,31 @@ library(tidyverse)
 
 view(CIACountries)
 
-g <- ggplot(data = CIACountries, aes(y = gdp, x = educ)) + geom_point((color=net_users),size = 3)
+g <- ggplot(data = CIACountries, aes(y = gdp, x = educ)) +
+  geom_point(alpha = 0.9,aes( size= roadways)) + 
+   coord_trans(y="log10") + facet_wrap(~net_users, nrow=1)+
+  theme(legend.position = "top")
 g
+
+
+ChargesNJ <- MedicareCharges %>%
+  filter(stateProvider == "NJ")
+ChargesNJ
+
+
+p <- ggplot(
+  data = ChargesNJ, 
+  aes(x = reorder(drg, mean_charge), y = mean_charge)
+) +
+  geom_col(fill = "gray") +
+  ylab("Statewide Average Charges ($)") + 
+  xlab("Medical Procedure (DRG)") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = rel(0.5))) +
+  geom_point(data = MedicareCharges, size = 1, alpha = 0.3)
+p
+
+
+ggplot(data = mosaicData::HELPrct, aes(x = homeless)) + 
+  geom_bar(aes(fill = substance), position = "fill") +
+  scale_fill_brewer(palette = "Spectral") + 
+  coord_flip()
