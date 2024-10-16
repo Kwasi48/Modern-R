498cec2f9e56880dbb20de8250a3db45cf647693
Kwasi48 <abuwestwood@gmail.com>
2024-10-16 03:38:38 +0000

    women names

--- file 1.R
+++ file 1.R
@@ -31,3 +31,57 @@ ggplot(data = mosaicData::HELPrct, aes(x = homeless)) +
   geom_bar(aes(fill = substance), position = "fill") +
   scale_fill_brewer(palette = "Spectral") + 
   coord_flip()
+
+g <- ggplot(data = SAT_2010, aes( x = expenditure, y = math)) + geom_point()+
+  geom_smooth(method = 'lm', se=FALSE) + 
+  xlab("Average expenditure per student ($1000)")+
+  ylab("Average score on math SAT")
+
+SAT_2010 <- SAT_2010 %>%
+  mutate(
+    SAT_rate = cut(
+      sat_pct, 
+      breaks = c(0, 30, 60, 100), 
+      labels = c("low", "medium", "high")
+    )
+  )
+
+g <- g %+% SAT_2010
+
+g <- g %+% SAT_2010
+g + aes(color = SAT_rate)
+g + facet_wrap(~SAT_rate)
+
+library(NHANES)
+ggplot(
+  data = slice_sample(NHANES, n = 1000), 
+  aes(x = Age, y = Height, color = fct_relevel(Gender, "male"))
+) + 
+  geom_point() + 
+  geom_smooth() + 
+  xlab("Age (years)") + 
+  ylab("Height (cm)") +
+  labs(color = "Gender")
+
+
+library(macleish)
+ggplot(data = whately_2015, aes(x = when, y = temperature)) + 
+  geom_line(color = "darkgray") + 
+  geom_smooth() + 
+  xlab(NULL) + 
+  ylab("Temperature (degrees Celsius)")
+
+
+whately_2015 |> 
+  mutate(month = as.factor(lubridate::month(when, label = TRUE))) |>
+  group_by(month) |>
+  skim(temperature) |>
+  select(-na)
+
+ggplot(data = whately_2015, aes(
+  x=lubridate::month(when, label=TRUE),
+  y=temperature
+)) + 
+  geom_boxplot()+ 
+  xlab("Month") +
+  ylab("Temperature(degree Celsius)")
