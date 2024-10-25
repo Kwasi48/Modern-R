64e9b0b4bb7209f5e2c9a0eda770f18235bd93a0
Kwasi48 <abuwestwood@gmail.com>
2024-10-25 20:28:27 +0000

    add up

--- data wrangling.R
+++ data wrangling.R
@@ -1,1 +1,17 @@
 #Data wrangling
+
+library(tidyverse)
+library(mdsr)
+presidential
+
+select(presidential, name, party)
+
+filter(presidential, party == "Republican")
+
+select(
+  filter(presidential, year(start) > 1973 & party == "Democratic"), name
+)
+
+my_presidents <- presidential |> 
+  mutate(term.length = interval(start, end) / dyears(1))
+my_presidents
