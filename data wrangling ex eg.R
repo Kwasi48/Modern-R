f1b9b064f5fd2beca6d564455b3507cb14bec798
Kwasi48 <abuwestwood@gmail.com>
2024-10-26 12:26:26 +0000

    new york metsd

--- data wrangling ex eg.R
+++ data wrangling ex eg.R
@@ -13,3 +13,34 @@ my_mets |>
 nrow(mets)
 
 select(filter(Teams, teamID == "NYN" & yearID %in% 2004:2012), yearID, teamID, W, L)
+
+#Improved Example
+Teams |> 
+  filter(teamID == "NYN" & yearID %in% 2004:2012) |>
+  select(yearID, teamID, W, L)
+
+
+mets_ben <- Teams |> 
+  select(yearID, teamID, W, L, R, RA) |>
+  filter(teamID == "NYN" & yearID %in% 2004:2012)
+
+mets_ben
+
+mets_ben <- mets_ben |>
+  rename(RS = R)
+mets_ben
+
+mets_ben <- mets_ben |>
+  mutate(WPct = W/ (W + L))
+
+mets_ben
+
+mets_ben <- mets_ben |>
+  mutate(WPct_hat = 1 / (1 + (RA/RS)^2))
+mets_ben
+
+mets_ben <- mets_ben |>
+  mutate(W_hat = WPct_hat * (W + L))
+mets_ben
+
+filter(mets_ben, W >= W_hat)
