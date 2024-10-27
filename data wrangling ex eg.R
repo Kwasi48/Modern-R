87b99d8552c47aaf0d624d3d923baee7a26bf462
Kwasi48 <abuwestwood@gmail.com>
2024-10-27 22:53:37 +0000

    updated a filee

--- data wrangling ex eg.R
+++ data wrangling ex eg.R
@@ -44,3 +44,137 @@ mets_ben <- mets_ben |>
 mets_ben
 
 filter(mets_ben, W >= W_hat)
+
+filter(mets_ben, W < W_hat)
+
+arrange(mets_ben, desc(WPct))
+
+mets_ben |>
+  mutate(Diff = W - W_hat) |>
+  arrange(desc(Diff))
+
+#summarise a single variable with the skim() command
+mets_ben |>
+  skim(W)
+
+mets_ben |>
+  summarise(
+    num_years = n(),
+    total_W = sum(W),
+    total_L = sum(L),
+    total_WPct = sum(W) / sum(W + L),
+    sum_resid = sum(W - W_hat)
+  )
+
+mets_ben <- mets_ben |>
+  mutate(
+    gm = ifelse(
+      yearID == 2004, 
+      "Duquette", 
+      ifelse(
+        yearID >= 2011, 
+        "Alderson", 
+        "Minaya")
+    )
+  )
+mets_ben
+
+mets_ben <- mets_ben |>
+  mutate(
+    gm = case_when(
+      yearID == 2004 ~ "Duquette",
+      yearID >= 2011 ~ "Alderson",
+      TRUE ~ "Minaya"
+    )
+  )
+
+mets_ben  |>
+  group_by(gm) |>
+  summarise(
+    num_years = n(),
+    total_W = sum(W),
+    total_L = sum(L),
+    total_WPct = sum(W) / sum(W + L),
+    sum_resid = sum(W - W_hat)
+  ) |>
+  arrange(desc(sum_resid))
+
+Teams |>
+  select(yearID, teamID, W, L, R, RA) |>
+  filter(teamID == "NYN" & yearID %in% 2004:2012) |>
+  rename(RS = R) |>
+  mutate(
+    WPct = W / (W + L), 
+    WPct_hat = 1 / (1 + (RA/RS)^2), 
+    W_hat = WPct_hat * (W + L), 
+    gm = case_when(
+      yearID == 2004 ~ "Duquette", 
+      yearID >= 2011 ~ "Alderson", 
+      TRUE ~ "Minaya"
+    )
+  ) |>
+  group_by(gm) |>
+  summarize(
+    num_years = n(), 
+    total_W = sum(W), 
+    total_L = sum(L),
+    total_WPct = sum(W) / sum(W + L), 
+    sum_resid = sum(W - W_hat)
+  ) |>
+  arrange(desc(sum_resid))
+
+Teams |>
+  select(yearID, teamID, franchID, W, L, R, RA) |>
+  filter(yearID %in% 2004:2012) |>
+  rename(RS = R) |>
+  mutate(
+    WPct = W / (W + L), 
+    WPct_hat = 1 / (1 + (RA/RS)^2), 
+    W_hat = WPct_hat * (W + L)
+  ) |>
+  group_by(franchID) |>
+  summarize(
+    num_years = n(), 
+    total_W = sum(W), 
+    total_L = sum(L),
+    total_WPct = sum(W) / sum(W + L), 
+    sum_resid = sum(W - W_hat)
+  ) |>
+  arrange(sum_resid) |>
+  head(6)
+
+
+library(babynames)
+head(babynames)
+
+Random_subset <-  babynames |>
+  filter( year %in% 1941:2010 )
+Random_subset
+
+babynames |> select(n > 100)
+babynames |> select(- year)
+babynames |> mutate(name_length = nchar(name))
+
+babynames |> sex == M |> select(-prop)
+
+babynames |> select(year, year, sex)
+
+babynames |> group_by(n) |> summarize(ave = mean(n))
+
+babynames |> group_by(n > 100) |> summarize(total = sum(n))
+
+library(tidyverse)
+mtcars |>
+  group_by(cyl) |>
+  summarize(avg_mpg = mean(mpg)) |>
+  filter(am == 1)
+
+Teams
+
+my_teams <- Teams |>
+  mutate( BA = H / AB,
+          total_bases =  )
+my_teams
+
+
+
--- multi data wrangling.R
+++ multi data wrangling.R
@@ -1,1 +1,18 @@
 #data wrangling on multiple 
+
+library(tidyverse)
+library(mdsr)
+library(nycflights13)
+glimpse(flights)
+
+head(airlines, 3)
+
+#inner join used here
+flights_joined <- flights |>
+  inner_join(airlines, by = c("carrier" = "carrier"))
+
+glimpse(flights_joined)
+
+flights_joined |>
+  select(carrier, name, flight, origin, dest) |> 
+  head(3)
