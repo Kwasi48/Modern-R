719c3edb1eed8c69f4385d93bded238431a48865
Kwasi48 <abuwestwood@gmail.com>
2024-12-13 13:21:33 +0000

    returning

--- iteration.R
+++ iteration.R
@@ -1,1 +1,22 @@
 #Iteration
+library(tidyverse)
+library(mdsr)
+library(Lahman)
+names(Teams)
+
+str(Teams)
+glimpse(Teams)
+
+averages <- NULL
+for (i in 15:40) {
+  averages[i -14] <- mean(Teams[,i], na.rm =TRUE)
+}
+names(averages) <- names(Teams)[15:40]
+averages
+
+a <- "a String"
+class(a)
+is.vector(a)
+length(a)
+
+exp(1:3)
