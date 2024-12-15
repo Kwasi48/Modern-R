c5e68b2f1c146a6e8583620003526e7813eace8e
Kwasi48 <abuwestwood@gmail.com>
2024-12-15 03:05:07 +0000

    hope it all make sense one day 😂

--- annual leaders.R
+++ annual leaders.R
@@ -17,3 +17,43 @@ hr_leader <- function(x){
 Teams |> 
   filter(yearID == 1961 & lgID == "AL") |>
   hr_leader()
+
+hr_leaders <- Teams |> 
+  group_by(yearID, lgID) |>
+  group_modify(~hr_leader(.x), .keep = TRUE)
+
+tail(hr_leaders,4)
+
+hr_leaders |>
+  group_by(lgID) |>
+  summarise(mean_hr = mean(HR))
+
+hr_leaders |>
+  filter(yearID >= 1916) |>
+  group_by(lgID) |>
+  summarise(mean_hr = mean(HR))
+
+hr_leaders |>
+  filter(yearID >= 1916) |>
+  ggplot(aes(x = yearID, y=HR, colour = lgID)) + 
+  geom_line() + 
+  geom_point() + 
+  geom_smooth( se = FALSE) +
+  geom_vline(xintercept = 1973) + 
+  annotate(
+    "Text", x = 1974, y = 25, 
+    label = "AL adopts DH", hjust = "left"
+  ) + 
+  labs(x='Year', y = "Home runs", color = "League")
+
+
+k_actual <- TeamRuns |> 
+  group_by(yearID) |> 
+  group_modify(~fit_k(.x))
+k_actual |>
+  ungroup() |>
+  skim(k)
+
+ggplot(data=k_actual,aes(x=k)) + 
+  geom_density() +
+  xlab("Best fit exponent for a single season")
