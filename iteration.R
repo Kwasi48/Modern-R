3a64000e7a82c4cead33f3ffd7bdc6482d9316d7
Kwasi48 <abuwestwood@gmail.com>
2024-12-15 02:03:30 +0000

    extended example

--- iteration.R
+++ iteration.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(mdsr)
 library(Lahman)
+library(bench)
 names(Teams)
 
 str(Teams)
@@ -20,3 +21,53 @@ is.vector(a)
 length(a)
 
 exp(1:3)
+
+if (c(TRUE, FALSE)) {
+  cat("This is a great book!")
+}
+
+x <- 1:1e5
+bench::mark(
+  exp(x),
+  map_dbl(x, exp)
+)
+
+Teams |> 
+  summarise(
+    across(
+      where(is.numeric), function(x) mean(x, na.rm = TRUE)
+    )
+  )
+Teams |>
+  select(15:40) |>
+  map_dbl(mean, na.rm=TRUE)
+
+
+angels <- Teams |> 
+  filter(franchID == "ANA") |> 
+  group_by(teamID, name) |>
+  summarize(began = first(yearID), ended = last(yearID), .groups = "drop") |> 
+  arrange(began)
+angels
+
+angels_names <- angels |>
+  pull(name)
+nchar(angels_names)
+
+
+top5 <- function(data, team_name) {
+  data |>
+    filter(name == team_name) |>
+    select(teamID, yearID, W, L, name) |>
+    arrange(desc(W)) |>
+    head(n = 5)
+}
+
+angels_names |>
+  map(top5, data=Teams)
+
+angels_names |>
+  map_dfr(top5, data=Teams) |>
+  group_by(teamID, name) |>
+  summarise(N = n(), means_wins= mean(W)) |>
+  arrange(desc(means_wins))
