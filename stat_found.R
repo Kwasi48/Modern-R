c95ad70e344572ea8378778e7a8b7d42f992aaf8
Kwasi48 <abuwestwood@gmail.com>
2024-12-20 13:34:11 +0000

    statistics foundation

--- stat_found.R
+++ stat_found.R
@@ -13,3 +13,74 @@ sf_25 <- SF |>
 
 sf_25 |>
   skim(arr_delay)
+
+SF |> 
+  skim(arr_delay)
+
+
+sf_25 |> 
+  summarise(q28 = quantile(arr_delay, p = 0.98))
+
+SF |> 
+  group_by(arr_delay < 90) |> 
+  count() |> 
+  mutate(pct = n / nrow(SF))
+
+
+SF |> 
+  summarise(q28 = quantile(arr_delay, p = 0.98))
+ 
+
+n  <- 25
+SF |> 
+  slice_sample(n = n) |>
+  summarise(mean_arr_delay = mean(arr_delay))
+
+SF |> 
+  slice_sample(n = n) |>
+  summarise(mean_arr_delay = mean(arr_delay))
+
+num_trials <- 500
+sf_25_means <- 1:num_trials |>
+  map_dfr(
+    ~SF |>
+      slice_sample(n = n) |>
+      summarise(mean_arr_delay = mean(arr_delay))
+  ) |>
+  mutate(n = n)
+head(sf_25_means)
+
+sf_25_means |>
+  skim(mean_arr_delay)
+
+sf_25_means |>
+  summarise(
+    x_bar = mean(mean_arr_delay),
+    se = sd(mean_arr_delay)
+  ) |>
+  mutate(
+    ci_lower = x_bar - 2 * se,
+    ci_upper = x_bar + 2 * se
+  )
+
+
+#T test
+sf_25_means |>
+  pull(mean_arr_delay) |>
+  t.test()
+
+n <- 100
+sf_100_means <- 1:500 |>
+  map_dfr(
+    ~SF |>
+      slice_sample(n = n) |>
+      summarise(mean_arr_delay = mean(arr_delay))
+  ) |> 
+  mutate(n = n)
+
+sf_25_means |> 
+  bind_rows(sf_100_means) |>
+  ggplot(aes(x = mean_arr_delay)) + 
+  geom_histogram(bins = 30) + 
+  facet_grid( ~ n) + 
+  xlab("Sample mean")
