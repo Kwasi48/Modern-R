18fad3c198a7f098ac510e9921112e69f0d46fa8
Kwasi48 <abuwestwood@gmail.com>
2024-12-22 06:11:37 +0000

    bootstrapping

--- stat_found.R
+++ stat_found.R
@@ -92,3 +92,22 @@ three_flights <- SF |>
 
 three_flights
 three_flights |> slice_sample(n = 3, replace = TRUE)
+
+n < - 200
+orig_sample <- SF |>
+  slice_sample(n = n, replace = FALSE)
+
+orig_sample |> 
+  slice_sample(n=n, replace = TRUE) |>
+  summarise(mean_arr_delay = mean(arr_delay))
+
+sf_200_bs <- 1:num_trials |> 
+  map_dfr(
+    ~orig_sample |> 
+      slice_sample( n = n, replace = TRUE) |>
+      summarise(mean_arr_delay = mean(arr_delay)) 
+  ) |>
+  mutate(n = n)
+
+sf_200_bs |> 
+  skim(mean_arr_delay)
